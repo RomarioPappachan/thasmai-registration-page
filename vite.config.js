064e1b7995b5fa26a8f8b755fbab66742d1d5556
Romario <romariopappachan@gmail.com>
2024-09-23 14:36:30 +0530

    install dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3030,
-  },
-  preview: {
-    port: 8080,
+    port: process.env.VITE_PORT || 3000, // Use port 7000 from .env or fallback to 3000
   },
 })
